e58bf54c0056deb355be34f253da3f074f187be8
unknown <singhnsatya@gmail.com>
2018-12-08 05:43:41 +0530

    update

--- food-exchange-ux/public/src/api/searchApi.js
+++ food-exchange-ux/public/src/api/searchApi.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://ec2-3-16-217-186.us-east-2.compute.amazonaws.com/api";
+axios.defaults.baseURL = "http://ec2-18-224-73-177.us-east-2.compute.amazonaws.com/api";
 
 const searchAPi = {
   async search(data) {
-    let endpoint = `/search?startDate=${data.startDate}&endDate=${data.endDate}&hour=${data.hour}`;
+    let endpoint = `v1/search?startDate=${data.startDate}&endDate=${data.endDate}&hour=${data.hour}`;
     return await axios.get(endpoint);
   }
 }
